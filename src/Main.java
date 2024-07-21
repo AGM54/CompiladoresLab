c455f44cf53ab7ba33fad0fa3e221c8b51ac6372
AGM54 <134453750+AGM54@users.noreply.github.com>
2024-07-20 19:51:59 -0600

    detección errores
    detección errores

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,19 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) throws Exception {
         String inputFile = Paths.get("src/test.txt").toAbsolutePath().toString();
+
+        // Lee el archivo de entrada
         CharStream input = CharStreams.fromFileName(inputFile);
 
+        // Crea el lexer y el parser
         ConfRoomSchedulerLexer lexer = new ConfRoomSchedulerLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         ConfRoomSchedulerParser parser = new ConfRoomSchedulerParser(tokens);
 
-
+        // Parse el archivo de entrada y obtén el árbol de sintaxis
         ParseTree tree = parser.prog();
+
+        // Crea el listener y el walker
         ConfRoomSchedulerSemanticChecker semanticChecker = new ConfRoomSchedulerSemanticChecker();
         ParseTreeWalker walker = new ParseTreeWalker();
         walker.walk(semanticChecker, tree);
